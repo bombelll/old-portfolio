3411dce630b9ca86b3f86d67905baa2338f5afb6
bombel <eryk.urbanskispoko@gmail.com>
2023-03-03 18:10:41 +0100

    add 404 page

---
+++ public/404/assets/js/app.js
@@ -0,0 +1,10 @@
+let typed = new Typed("#js-info", {
+    strings: [
+        "404, page not found."
+    ],
+    typeSpeed: 60,
+})
+
+setTimeout(() => {
+    window.location.href = "https://bombel.top";
+}, 5000);
